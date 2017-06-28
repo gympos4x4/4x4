adac2a067f39ecfcab32017941a900a4ac706631
pridilla <ptrridilla9@gmail.com>
2017-06-28 18:08:09 +0200

    Add files via upload

---
+++ Car/MotorControl.h
@@ -0,0 +1,24 @@
+class MotorControl{
+  
+  public:
+  
+    static void init(int motorPin1, int motorPin2, int enablePin){
+      pinMode(motorPin1, OUTPUT);
+      pinMode(motorPin2, OUTPUT);
+      pinMode(enablePin, OUTPUT);
+    }
+    
+    static void go(int speed, int motorPin1, int motorPin2, int enablePin){
+      analogWrite(enablePin, speed);
+      int temp = 0;
+      if(speed > 0){
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+      }
+      else{
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+      }
+    }
+};
+
